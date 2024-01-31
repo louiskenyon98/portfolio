9e75c5474186fa064eee764b1d359eca7ba0bd39
Louis Kenyon <louis.kenyon@kenyoncomputing.co.uk>
2024-01-31 18:12:16 +0000

    Assign stocks their correct weightings

--- portfolio.py
+++ portfolio.py
@@ -3,13 +3,14 @@ import yfinance as yf
 
 selection = ["NVDA", "AAPL", "GOOG", "ADBE"]
 allocation = [0.3, 0.2, 0.4, 0.1]
-test = [{"stock" : selection[i], "allocation": allocation[i]} for i in range(len(selection))]
-portfolio = yf.download(selection, start="2012-01-01", end="2024-01-24")[['Adj Close']]
 
+# Assigns each respective stock its chosen weighting and then places them in alphabetical order so when the yfinance data comes back they can be assigned to the correct weights
+weighted_portfolio = sorted([{"stock" : selection[i], "weight": allocation[i]} for i in range(len(selection))], key=lambda item: item["stock"])
+
+portfolio = yf.download(selection, start="2012-01-01", end="2024-01-24")[['Adj Close']]
 
 i = 0
 # Compute normalised returns for each stock
-print(portfolio.columns)
 for ticker in portfolio.columns:
     normalised_returns = []
     alloc_returns = []
@@ -18,8 +19,7 @@ for ticker in portfolio.columns:
     portfolio[f"Norm. Return {ticker[1]} "] = normalised_returns
 
     for price in normalised_returns:
-        # print('normalised return ', price)
-        alloc_returns.append(price*allocation[i])
+        alloc_returns.append(price*weighted_portfolio[i]["weight"])
     portfolio[f"Alloc. Return {ticker[1]} "] = alloc_returns
 
     i += 1
