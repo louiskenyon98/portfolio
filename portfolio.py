98cad0015aa8c2657c1ac068ca3893092f57035a
Louis Kenyon <louiskenyon98@gmail.com>
2024-02-05 13:49:17 +0000

    Refactor loop to use vector operations

--- portfolio.py
+++ portfolio.py
@@ -3,25 +3,27 @@ import yfinance as yf
 
 selection = ["NVDA", "AAPL", "GOOG", "ADBE"]
 allocation = [0.3, 0.2, 0.4, 0.1]
+init_position = 10000
 
 # Assigns each respective stock its chosen weighting and then places them in alphabetical order so when the yfinance data comes back they can be assigned to the correct weights
 weighted_portfolio = sorted([{"stock" : selection[i], "weight": allocation[i]} for i in range(len(selection))], key=lambda item: item["stock"])
 
 portfolio = yf.download(selection, start="2012-01-01", end="2024-01-24")[['Adj Close']]
 
-i = 0
 # Compute normalised returns for each stock
-for ticker in portfolio.columns:
-    normalised_returns = []
-    alloc_returns = []
-    for price in (portfolio[ticker]):
-        normalised_returns.append(price / portfolio[ticker].iloc[0])
-    portfolio[f"Norm. Return {ticker[1]} "] = normalised_returns
+for index, ticker in enumerate(portfolio.columns):
+    norm_return_col = f"Norm. Return {ticker[1]}"
+    alloc_return_col = f"Alloc Return {ticker[1]}"
 
-    for price in normalised_returns:
-        alloc_returns.append(price*weighted_portfolio[i]["weight"])
-    portfolio[f"Alloc. Return {ticker[1]} "] = alloc_returns
+    norm_returns = portfolio[ticker] / portfolio[ticker].iloc[0]
+    portfolio[norm_return_col] = norm_returns
 
-    i += 1
+    portfolio[alloc_return_col] = norm_returns * weighted_portfolio[index]["weight"]
+
+del allocation, index, selection, ticker, norm_returns, alloc_return_col, norm_return_col
+
+#Compute portfolio value of x amount invested over lifetime
+portfolio['Total Position'] = portfolio.apply(lambda row: print('ello: ', row))
+
 
 print(portfolio.head())
